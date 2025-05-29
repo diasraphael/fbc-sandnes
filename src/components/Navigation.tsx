769f4c103a7fd254532cf8620f9af8213c34b718
Dias Raphael <diasraphael88@gmail.com>
2025-05-29 12:00:04 +0200

    navigation hover effects

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -7,20 +7,40 @@ export default function Navigation() {
         <div className="flex items-center justify-between h-20">
           <div className="text-white text-2xl font-bold">FBC Sandnes</div>
           <div className="hidden md:flex space-x-8">
-            <a href="#" className="text-white hover:text-gray-200">
-              Home
+            <a
+              href="#"
+              className="relative text-white hover:text-[#832732] transition-colors duration-500 ease-out group"
+            >
+              <span className="relative z-10">Home</span>
+              <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[#832732] transition-all duration-500 ease-out group-hover:w-full"></span>
             </a>
-            <a href="#" className="text-white hover:text-gray-200">
-              About
+            <a
+              href="#"
+              className="relative text-white hover:text-[#832732] transition-colors duration-500 ease-out group"
+            >
+              <span className="relative z-10">About</span>
+              <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[#832732] transition-all duration-500 ease-out group-hover:w-full"></span>
             </a>
-            <a href="#" className="text-white hover:text-gray-200">
-              Sermons
+            <a
+              href="#"
+              className="relative text-white hover:text-[#832732] transition-colors duration-500 ease-out group"
+            >
+              <span className="relative z-10">Sermons</span>
+              <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[#832732] transition-all duration-500 ease-out group-hover:w-full"></span>
             </a>
-            <a href="#" className="text-white hover:text-gray-200">
-              Events
+            <a
+              href="#"
+              className="relative text-white hover:text-[#832732] transition-colors duration-500 ease-out group"
+            >
+              <span className="relative z-10">Events</span>
+              <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[#832732] transition-all duration-500 ease-out group-hover:w-full"></span>
             </a>
-            <a href="#" className="text-white hover:text-gray-200">
-              Contact
+            <a
+              href="#"
+              className="relative text-white hover:text-[#832732] transition-colors duration-500 ease-out group"
+            >
+              <span className="relative z-10">Contact</span>
+              <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[#832732] transition-all duration-500 ease-out group-hover:w-full"></span>
             </a>
           </div>
         </div>
